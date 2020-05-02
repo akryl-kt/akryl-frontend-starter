fe9bf8754d305f7202fdf766325435873415e861
Alexander Shilov <aleksandr.schilow2012@gmail.com>
2020-05-02 14:27:08 +0300

    Rename HTML tag names to lowerCamelCase

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -1,12 +1,12 @@
 package app
 
 import io.akryl.component
-import io.akryl.dom.html.H1
+import io.akryl.dom.html.h1
 import react_dom.ReactDom
 import kotlin.browser.document
 
 fun app() = component {
-  H1(text = "Hello, World!")
+  h1(text = "Hello, World!")
 }
 
 fun main() {
